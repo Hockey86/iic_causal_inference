77d5411c176687ba2e65a851d4bd7ad9212daeca
Hockey86 <hockeysun86@gmail.com>
2021-01-13 23:23:39 -0500

    finish initial Yd evaluation

--- aim1/step8_evaluate_potential_outcome.py
+++ aim1/step8_evaluate_potential_outcome.py
@@ -79,18 +79,61 @@ def evaluate_one_patient(i, D, Dname, Pobs, C, cluster, responses, simulator, ou
     # predict outcome
     
     Xsim = [e[r].mean() for r in responses]
-    #sim_name = [r+'_mean' for r in responses]
     
     Xdrug = np.array(d)
     Xdrug[Xdrug<1e-6] = np.nan
     Xdrug = np.nanmean(Xdrug, axis=0)
     Xdrug[np.isnan(Xdrug)] = 0
-    #Dname2 = ['mean_positive_dose_'+x for x in Dname]
 
     # create X and y
     X = np.r_[Xdrug, Xsim, C[i]]
-    #Xnames = Dname2 + sim_name + Cname
     yp = outcome_model.predict(X.reshape(1,-1))[0]
+    
+    return yp
+
+
+def evaluate_one_patient2(i, D, Dname, Pobs, C, cluster, responses, simulator, outcome_model, drug_regime_func, AR_p):
+    # use simualtor to generate e
+    # use drug_regime_func to generate d
+    T, ND = D[i].shape
+    d = [np.zeros(ND)]*(AR_p-1)
+    e = {r:[np.tile(Pobs[r][i][:AR_p], (500,1))] for r in responses}
+    A = {}
+    for t in range(AR_p, T):#tqdm()
+        # Dt = f(E1:t, D1:t-1, C)
+        d.append(drug_regime_func([e[r] for r in responses], d, C[i], Dname, i, t))
+        
+        # Et+1 = g(E1:t, D1:t, C)
+        for r in responses:
+            if t==AR_p:
+                A[r] = logit(np.clip(Pobs[r][i][:AR_p], 1e-6, 1-1e-6)).reshape(1,-1)
+            Esim, A[r] = simulator[r].predict(
+                    [np.r_[np.array(d), np.zeros((1,ND))]],
+                    cluster[[i]], sid_index=[i],
+                    Astart=A[r], return_A=True,
+                    verbose=False)
+            Esim = Esim[0][:,[-1]]
+            e[r].append(Esim)
+    d.append(np.zeros(ND)+np.nan) # fill the last drug with NaN, so not counted
+            
+    d = np.array(d)
+    e = {r:np.concatenate(e[r], axis=1) for r in responses}
+    
+    # predict outcome
+    
+    Xsim = np.array([e[r].mean(axis=1) for r in responses]).T
+    
+    Xdrug = np.array(d)
+    Xdrug[Xdrug<1e-6] = np.nan
+    Xdrug = np.nanmean(Xdrug, axis=0)
+    Xdrug[np.isnan(Xdrug)] = 0
+    Xdrug = np.tile(Xdrug, (500,1))
+
+    XC = np.tile(C[i], (500,1))
+
+    # create X and y
+    X = np.c_[Xdrug, Xsim, XC]
+    yp = outcome_model.predict(X)
     
     return yp
     
@@ -125,7 +168,7 @@ if __name__=='__main__':
             os.path.join(SIMULATOR_PATH, f'model_{simulator_model_type}.stan'),
             W, T0=[AR_p, MA_q], random_state=random_state)
         #model_path = os.path.join(SIMULATOR_PATH, f'results_{response}/model_fit_{data_type}_{response}_{simulator_model_type}{AR_p},{MA_q}_iter{max_iter}.pkl')
-        model_path = os.path.join('/data/IIC-Causality', f'model_fit_{data_type}_{response}_{simulator_model_type}{AR_p},{MA_q}_iter{max_iter}.pkl')
+        model_path = os.path.join('/data/HaoqiSun', f'model_fit_{data_type}_{response}_{simulator_model_type}{AR_p},{MA_q}_iter{max_iter}.pkl')
         simulator[response].load_model(model_path)
     
     ## load outcome prediction model
@@ -139,7 +182,7 @@ if __name__=='__main__':
     drug_regimes = {
         'always_zero':drug_from_constant(0, Dmax=Dmax),
         'always_propofol_0.01':drug_from_constant(1, Dmax=Dmax, drug='propofol'),
-        #'actual_drug':drug_from_data(D, Dmax=Dmax),
+        'actual_drug':drug_from_data(D, Dmax=Dmax),
     }
     
     ## for each drug regime, evaluate drug regime
@@ -150,14 +193,19 @@ if __name__=='__main__':
         
         # for each subject
         with Parallel(n_jobs=n_jobs, verbose=False) as par:
-            Yd[regime_name] = par(delayed(evaluate_one_patient)(i, D, Dname, Pobs, C, cluster, responses, simulator, outcome_model, drug_regime_func, AR_p) for i in tqdm(range(N)))
-        """
-        Yd[regime_name] = []
-        for i in tqdm(range(N)):
-            yp = evaluate_one_patient(i, D, Dname, Pobs, C, cluster, responses, simulator, outcome_model, drug_regime_func, AR_p)
-            Yd[regime_name].append(yp)
-        """
-            
+            res = par(delayed(evaluate_one_patient2)(
+                    i, D, Dname, Pobs, C, cluster,
+                    responses, simulator, outcome_model,
+                    drug_regime_func, AR_p) for i in tqdm(range(N)))
+        Yd[regime_name] = np.array(res)
         print(f'Y({regime_name}) = {np.mean(Yd[regime_name])}')
+
+        with open('res_evaluate_Yd.pickle', 'wb') as ff:
+            pickle.dump(Yd, ff)
     import pdb;pdb.set_trace()
         
+"""
+ids2=(C[:,1]<=55)&(C[:,-5]<=8.5)&(C[:,-4]<=7)&(C[:,4]==0)&(C[:,15]==0)&(C[:,20]==0)
+ids2.sum()
+{k:Yd[k][ids2].mean() for k in Yd}
+"""
